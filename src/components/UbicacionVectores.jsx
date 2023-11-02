9291f074d867f37c9bee41927c13c77ddc973ff4
Fernando Agüero <afernandodan@gmail.com>
2023-11-02 16:27:20 -0300

    se añadio un mensaje cuando no hay vectores en una ubicacion y cuando el vector no esta enfermo

--- src/components/UbicacionVectores.jsx
+++ src/components/UbicacionVectores.jsx
@@ -16,18 +16,22 @@ const UbicacionVectores = ({vectores}) => {
   }, [vectores])
 
   return <>
-    {vectoresConEnfermedades && vectoresConEnfermedades.map(vector => {
+    {vectoresConEnfermedades.length > 0 ? vectoresConEnfermedades.map(vector => {
       return <div key={vector.id}>
         <h3 style={{fontSize: "1.25rem"}}>{vector.tipo}</h3>
         <h3 style={{marginTop: "0"}}><i>enfermedades</i>:</h3>
         <ul style={{listStyleType: "none", marginTop: "0.25rem"}}>
-          {vector.enfermedades && vector.enfermedades.map((enfermedad, idx, arr) => {
+          {vector.enfermedades.length > 0 ? vector.enfermedades.map((enfermedad, idx, arr) => {
             const {id, nombre, patogeno} = enfermedad;
             return <li key={id}>{nombre} ({patogeno.tipo})</li>
-          })}
+          }):
+            <li>No tiene enfermedades</li>
+          }
         </ul>
       </div>
-    })}
+    }) :
+      <p>No hay vectores en la ubicación</p>
+    }
   </>
 }
 
